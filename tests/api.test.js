552fd01321e11d77bc12fdcf5fb612a8786537e7
Sean McPherson <seanmcp25@gmail.com>
2017-09-08 14:17:28 -0400

    Add style to button

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,19 +1,6 @@
 const request = require('supertest')
 const app = require('../server')
 
-// describe('GET /api/bro', function() {
-//   test('Should retrieve id and username successfully', function() {
-//     return request(app)
-//       .get('/api/bro')
-//       .expect(200)
-//       .then(function(res) {
-//         expect(res.body).toHaveProperty('id')
-//         expect(res.body).toHaveProperty('username')
-//         expect(res.body.username).toBe('seanmcp')
-//       })
-//   })
-// })
-
 describe('GET /', function() {
   test('Should send object sucessfully', function() {
     return request(app)
@@ -31,21 +18,10 @@ describe('GET /', function() {
 describe('GET /me', function() {
   test('Should send object sucessfully', function() {
     return request(app)
-      .get('/me')
+      .get('/api/me')
       .expect(200)
   })
 })
-
-// describe('GET /me', function() {
-//   test('Should return a json object', function() {
-//     return request(app)
-//       .get('/me')
-//       .expect(200)
-//       .then(function(res) {
-//         expect(res.body).toBeTruthy()
-//       })
-//   })
-// })
 
 describe('GET /decks', function() {
   test('Should send object containing all decks sucessfully', function() {
